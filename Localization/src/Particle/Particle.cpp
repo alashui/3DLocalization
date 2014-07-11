07200fcbfdc7bab8a8eebfa3414b6c63d3045f99
jhallard <jhallard@ucsc.edu>
2014-07-11 15:24:21 -0700

    Particle class seems to be finished right now. More functionality can be added later but essentailly it is finished, also updated Perspective struct to include a ToVector function

--- Localization/src/Particle/Particle.cpp
+++ Localization/src/Particle/Particle.cpp
@@ -1,1 +1,83 @@
-Particle.cpp
+/**
+*	@File   - Particle.cpp
+*	@Author - John Allard, Alex Rich. Summer 2014.
+*	@Info   - This is the definition of the Particle class for use by the 3DLocalization program, declared in the Particle.h file. This file will define all
+*			of then public and private member functions for the class. For a more in-depth description of this class, see it's section 
+*			in the Documentation/CodeDocumentation directory.
+**/
+
+
+#include "Particle.h"
+
+namespace MCL
+{
+
+	Particle::Particle() :
+	weightmin(0), weightmax(100), defaultweight(10), perspective()
+	{
+		weight = defaultweight;
+	}
+
+	Particle::Particle(Perspective p) :
+	weightmin(0), weightmax(100), defaultweight(10), perspective(p)
+	{
+		weight = defaultweight;
+	}
+
+	Particle(Perspective p, float w) :
+	weightmin(0), weightmax(100), defaultweight(10), perspective(p)
+	{
+		if(w >= weightmin && w <= weightmax)
+			weight = w;
+		else
+			weight = defaultweight;
+	}
+
+	//====Destructor====//
+	~Particle()
+	{
+		// HOLDER FUNCTION
+	}
+
+
+	//====Get and Set====//
+	bool Particle::SetPerspective(Perspective p)
+	{
+		this->perspective = p;
+	}
+
+	MCL::Perspective Particle::getPerspective() const
+	{
+		return this->perspective;
+	}
+
+	float Particle::getPerspective(int index) const
+	{
+		if(index >= 0 && index < 6)
+		{
+			std::vector<float> temp = perspective.ToVector();
+			return 
+		}
+		else
+			throw std::logic_error("Invalid Index in GetPerspective Call")
+	}
+
+	float Particle::getWeight() const
+	{
+		return this->weight;
+	}
+
+	bool Particle::setWeight(float)
+	{
+		if(w >= weightmin && w <= weightmax)
+		{
+			weight = w;
+			return true;
+		}
+		else
+			return false;
+	}
+
+
+
+}
--- Localization/src/Particle/Particle.h
+++ Localization/src/Particle/Particle.h
@@ -1,1 +1,69 @@
-Particle.h
+/**
+*	@File   - Particle.h
+*	@Author - John Allard, Alex Rich. Summer 2014.
+*	@Info   - This is the declaration of the Particle class, a class that is used extensively by the 3DLocalization program. This class is declared inside
+*			of this file and defined inside of Particle.cpp. The Particle class will be used to represent a single guess as to where our robot could be in 
+			the environment. This 'guess' data structure will contain general data about the perspective 
+*
+**/
+
+#ifndef MCL_PARTICLE_H_
+#define MCL_PARTICLE_H_
+
+namespace MCL
+{
+
+	class Particle
+	{
+
+	private:
+	//********---------------------********//
+	//*****-- Private Member Fields --*****//
+	//********---------------------********//
+		MCL::Perspective perspective;
+
+		float weight;
+
+	//********------------------------********//
+	//*****-- Private Member Functions --*****//
+	//********------------------------********//
+
+
+	public:
+	//**********------------**********//
+	//*****-- Public Functions --*****//
+	//**********------------**********//
+
+		//====Constructor====//
+		Particle();
+
+		Particle(Perspective);
+
+		Particle(Perspective, float)
+
+		//====Destructor====//
+		~Particle();
+
+
+		//====Get and Set====//
+		bool SetPerspective(Perspective);
+
+		MCL::Perspective getPerspective() const;
+
+		float getPerspective(int) const;  // Throws std::logic_error if argument is < 0 or > 5
+
+		float getWeight() const;
+
+		bool setWeight(float);
+
+	//********-----------------------------------------------*******//
+	//*****-- Public Definitions, Constants, and other Fields--*****//
+	//********-----------------------------------------------*******//
+		const float weightmin;
+		const float weightmax;
+		const float defaultweight;
+	};
+
+}
+
+#endif

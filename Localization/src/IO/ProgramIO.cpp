348c1a9ddf78637751d3347b83506a970a8603ca
jhallard <jhallard@ucsc.edu>
2014-07-11 10:53:31 -0700

    Began designing and documenting the ProgramIO.h and .cpp files. Declared but haven't defined  3 IO functions

---
+++ Localization/src/IO/ProgramIO.cpp
@@ -0,0 +1,16 @@
+/**
+*	@File   - ProgramIO.cpp
+*	@Author - John Allard and Alex Rich. Summer 2014.
+*	@Info   - This file contains the definitions of the Input/Output related functions defined in the ProgramIO.h file. for more information on the purpose
+*			of these functions see either the code documentation of the ProgramIO.h file.
+*
+**/
+
+#include "ProgramIO.h"
+
+
+namespace 3DL_io
+{
+
+
+}

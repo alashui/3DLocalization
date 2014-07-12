172f71925964d6c75bd1b94c1c79d0ba8a192172
aarich <arich@hmc.edu>
2014-07-12 10:06:28 +1000

    structs have members

--- Localization/src/HelperStructs/Characterizer.h
+++ Localization/src/HelperStructs/Characterizer.h
@@ -1,1 +1,15 @@
-Characterizer.h
+// Characterizer.h
+
+#include "opencv2/core/core.hpp"
+#include <cstdlib>
+
+using namespace cv;
+using namespace std;
+
+struct characterizer {
+    Mat image;
+    Mat bw;
+    Mat gs;
+    Mat surfs;
+    Mat sifts;
+}
--- Localization/src/HelperStructs/Perspective.h
+++ Localization/src/HelperStructs/Perspective.h
@@ -1,1 +1,23 @@
-Perspective.h
+// Perspective.h
+
+#include <cstdlib>
+
+struct perspective
+{
+    float x;
+    float y;
+    float z;
+    float thetax;
+    float thetay;
+    float thetaz;
+
+    perspective(vector<float> v)
+    {
+    	x = v[0];
+    	y = v[1];
+    	z = v[2];
+    	thetax = v[3];
+    	thetay = v[4];
+    	thetaz = v[5];
+    }
+}

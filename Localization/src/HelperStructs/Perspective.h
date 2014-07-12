5059db9110ccb2acf4325deab88dd02730f65d1e
jhallard <jhallard@ucsc.edu>
2014-07-11 21:07:19 -0700

    Changed default Perspective.dx value in default constructor to 1. A default camera view vector of [0 0 0] might cause problems

--- Localization/src/HelperStructs/Perspective.h
+++ Localization/src/HelperStructs/Perspective.h
@@ -15,7 +15,7 @@ namespace MCL
 
         Perspective(std::vector<float> v) : x(v[0]), y(v[1]), z(v[2]), dx(v[3]), dy(v[4]), dz(v[5]) { }
         Perspective(Perspective p) : x(p.x)_, y(p.y), z(p.z), dx(p.dx), dy(p.dy), dz(p.dz) { }
-        Perspective() : x(0), y(0), z(0), dx(0), dy(0), dz(0) { }
+        Perspective() : x(0), y(0), z(0), dx(1), dy(0), dz(0) { }
 
         std::vector<float> ToVector() 
         {

1a4fd83246e12a45f3957ebf81b3004c04127a8f
aarich <arich@hmc.edu>
2014-07-15 16:14:07 +1000

    RobotState functionality

--- Localization/src/Robot/RobotState.cpp
+++ Localization/src/Robot/RobotState.cpp
@@ -1,1 +1,54 @@
-RoboState: Episode CPP
+/**
+* 
+*   @File RobotState.cpp
+*   @Author Alex Rich and John Allard, Summer 2014
+*   @Info A class to contain robot state information, continually updated.
+*
+**/
+
+#include "RobotState.h"
+
+
+using namespace cv;
+using namespace std;
+
+namespace MCL
+{
+    void generateCharacterizer(Mat& img)
+    {
+        int divs = 50;
+
+        vector<KeyPoint> kps;
+        Mat descs;
+
+        SurfFeatureDetector SurfDetector (3000, 6, 2, true, true);
+        SurfDescriptorExtractor SurfExtractor;
+        SurfDetector.detect(img, kps);
+        SurfExtractor.compute(img, kps, descs);
+        this->c.surfs = descs;
+
+        SiftFeatureDetector SiftDetector (800);
+        SiftDescriptorExtractor SiftExtractor;
+        SiftDetector.detect(img, kps);
+        SiftExtractor.compute(img, kps, descs);
+        this->c.sifts = descs;
+
+        Mat grs = averageImage::getPixSumFromImage(img, divs);
+
+        this->c.gs = grs;
+        this->c.bw = averageImage::aboveBelow(grs);
+        this->image = img;
+    }
+
+    Characterizer getCharacterizer() 
+    { return this->c; }
+
+    void setCharacterizer(Characterizer cnew) 
+    { this->c = cnew; }
+
+    Perspective getPerspective() 
+    { return this->p; }
+
+    void setPerspective(Perspective newp)
+    { this->p = newp; }
+}
--- Localization/src/Robot/RobotState.h
+++ Localization/src/Robot/RobotState.h
@@ -1,1 +1,40 @@
-RoboState
+/**
+* 
+*   @File RobotState.h
+*   @Author Alex Rich and John Allard, Summer 2014
+*   @Info A class to contain robot state information, continually updated.
+*
+**/
+
+#ifndef MCL_ROBOTSTATE_H_
+#define MCL_ROBOTSTATE_H_
+
+#include "../Helpers/Characterizer.h"
+#include "../Helpers/Perspective.h"
+
+#include "../../../PreLocalization/DatabaseGen/src/AverageImage.h"
+
+namespace MCL
+{
+    class RobotState
+    {
+    private:
+
+        Characterizer c;
+        Perspective p;
+
+    public:
+        RobotState();
+        ~RobotState();
+
+        Characterizer getCharacterizer();
+        void setCharacterizer(Characterizer);
+
+        Perspective getPerspective();
+        void setPerspective(Perspective);
+
+        void generateCharacterizer(Mat);
+    };
+}
+
+#endif

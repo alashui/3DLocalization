52556cbb10094ca8b0d46e1bac63f9f59fa7dbfa
aarich <arich@hmc.edu>
2014-07-19 18:47:22 +1000

    removing duplicate directory

--- Localization/KinectTest/build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/robotics/Desktop/3DLocalization/Localization/KinectTest/build/gtest
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
